d62873adc2f5d284908944460d1844bfdd8a860c
timiyay <tim@redronin.io>
2018-05-30 02:43:07 +1000

    Support float zooms in bounds() function (#19)
    * Strips trailing whitespace from package.json
    
    This formatting change was generated automatically when running
    `npm install` with npm v6.1.0.
    
    * Upgrades node-sphericalmercator to v1.1.0
    
    This version now supports float-based zoom levels.
    
    * Adds test for float zoom input for bounds function
    
    * Refactors expected test values to be more explicit
    
    * Rebuilds dist version of geo-viewport.js with latest sphericalmercator

--- test/viewport.js
+++ test/viewport.js
@@ -60,9 +60,24 @@ test('bounds for 512px tiles', function (t) {
     var xMax = bounds[2];
     var yMax = bounds[3];
 
-    t.equal(precisionRound(xMin, decDegreesFloatTolerance), precisionRound(-77.03945338726044, decDegreesFloatTolerance));
-    t.equal(precisionRound(yMin, decDegreesFloatTolerance), precisionRound(38.89697827424865, decDegreesFloatTolerance));
-    t.equal(precisionRound(xMax, decDegreesFloatTolerance), precisionRound(-77.03365981578827, decDegreesFloatTolerance));
-    t.equal(precisionRound(yMax, decDegreesFloatTolerance), precisionRound(38.89843950894583, decDegreesFloatTolerance));
+    t.equal(precisionRound(xMin, decDegreesFloatTolerance), -77.03945339);
+    t.equal(precisionRound(yMin, decDegreesFloatTolerance), 38.89697827);
+    t.equal(precisionRound(xMax, decDegreesFloatTolerance), -77.03365982);
+    t.equal(precisionRound(yMax, decDegreesFloatTolerance), 38.89843951);
     t.end();
+});
+
+test('bounds for float zooms', function (t) {
+  var zoom = 16.52;
+  var bounds = viewport.bounds([-77.036556, 38.897708], zoom, [1080, 350], 512);
+  var xMin = bounds[0];
+  var yMin = bounds[1];
+  var xMax = bounds[2];
+  var yMax = bounds[3];
+
+  t.equal(precisionRound(xMin, decDegreesFloatTolerance), -77.04059627);
+  t.equal(precisionRound(yMin, decDegreesFloatTolerance), 38.89668897);
+  t.equal(precisionRound(xMax, decDegreesFloatTolerance), -77.03251573);
+  t.equal(precisionRound(yMax, decDegreesFloatTolerance), 38.89872702);
+  t.end();
 });
